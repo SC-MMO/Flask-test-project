1be3b53c2bb5c02517ce7f28600051a90021a538
SSC-MMO <moritz.mummert@scaltel.de>
2025-06-04 11:55:11 +0200

    modified:   hello.py 	new file:   static/style.css

--- hello.py
+++ hello.py
@@ -1,27 +1,70 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Index Page'
+from flask import render_template
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
+# @app.route('/')
+# def index():
+#     return 'index'
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
+# @app.route('/login')
+# def login():
+#     return 'login'
 
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
+# @app.route('/user/<username>')
+# def profile(username):
+#     return f'{username}\'s profile'
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('login'))
+#     print(url_for('login', next='/'))
+#     print(url_for('profile', username='John Doe'))
+#     print(url_for('static', filename='style.css'))
+
+
+
+
+
+
+
+
+
+
+# @app.route('/')
+# def redirect_to_home():
+#     return redirect('/home')
+
+# @app.route('/home')
+# def home():
+#     return 'The home page'
+
+# @app.route('/hello')
+# def hello():
+#     return 'Hello, World'
+
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return f'User {escape(username)}'
+
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     # show the post with the given id, the id is an integer
+#     return f'Post {post_id}'
+
+# @app.route('/path/<path:subpath>')
+# def show_subpath(subpath):
+#     # show the subpath after /path/
+#     return f'Subpath {escape(subpath)}'
+
+# @app.route('/projects/')
+# def projects():
+#     return 'The project page'
