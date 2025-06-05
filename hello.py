68cdc8d4e75fc2dbd8397edf3fb6368e0de40f64
SSC-MMO <moritz.mummert@scaltel.de>
2025-06-05 11:58:14 +0200

    implemented toastr for popup msgs

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
-import logging
+import logging, secrets, json
 from flask import Flask, redirect, url_for, render_template, request, make_response, abort, Response, session, flash
 from werkzeug.utils import secure_filename
 from time import sleep
-import secrets
 from functools import wraps
+from user import user
+from flask_toastr import Toastr
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
@@ -11,7 +12,7 @@ app.logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('[%(asctime)s] %(levelname)s in %(module)s: %(message)s')
 
-
+toastr = Toastr(app)
 #* Decorators
 
 def dec(func):
@@ -28,9 +29,38 @@ def dec(func):
 
 #* Routes
 
+@app.route('/sign-up', methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        req = request.form
+        new_user = user(
+            username=req.get('username'), 
+            email=req.get('email'), 
+            password=req.get('password')
+        )
+        new_user_dict = new_user.to_dict()
+
+        with open("./users.json", "r") as my_file:
+            users = json.load(my_file)
+
+        if not any(u.get('username') == new_user.username for u in users):
+            users.append(new_user_dict)
+            app.logger.debug(users)
+            with open("./users.json", "w") as my_file:
+                json.dump(users, my_file, indent=4)
+
+            session['username'] = new_user.username
+            return redirect('/')
+        
+        flash("Username already exists", "error")
+        return redirect('/sign-up')
+
+    return render_template("sign_up.html")
+
 @app.route('/test')
 @dec
 def test():
+    flash('testflash')
     return "Test completed"
 
 @app.route('/hello/')
@@ -49,9 +79,18 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 @dec
 def login():
+    if str(session.get('username')) != 'None':
+        return redirect('/')
     if request.method == 'POST':
-        username = request.form.get('username')
-        session['username'] = username
+        req = request.form
+        a_user = user(username=req.get('username'), password=req.get('password'), email="N/A").__dict__
+        with open("./users.json", "r") as my_file:
+            users = json.load(my_file)
+            for u in users:
+                if a_user.get('username') == u.get('username') and a_user.get('password') == u.get('password'):
+                    session['username'] = req.get('username')
+                    break
+
         return redirect(url_for('index'))
 
     return render_template('login.html')
@@ -60,6 +99,7 @@ def login():
 @dec
 def logout():
     session.pop('username', None)
+    flash('logged out')
     return redirect(url_for('login'))
 
 @app.route('/admin')
