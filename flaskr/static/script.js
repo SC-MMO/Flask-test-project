e5666467b902680cef5d00d61154c6f4bcb59944
SSC-MMO <moritz.mummert@scaltel.de>
2025-06-06 09:21:07 +0200

    commented out deprecated show toast

--- flaskr/static/script.js
+++ flaskr/static/script.js
@@ -1,17 +1,17 @@
 const toastNotification = document.querySelector('#toastNotification');
 
-const showToast = (message, isError = false) => {
-    let toast = document.createElement('div');
-    toast.classList.add('toast');
-    if (isError) toast.classList.add('error');
+// const showToast = (message, isError = false) => {
+//     let toast = document.createElement('div');
+//     toast.classList.add('toast');
+//     if (isError) toast.classList.add('error');
 
-    toast.innerHTML = message;
-    toastNotification.appendChild(toast);
+//     toast.innerHTML = message;
+//     toastNotification.appendChild(toast);
 
-    setTimeout(() => {
-        toast.remove();
-    }, 5000);
-}
+//     setTimeout(() => {
+//         toast.remove();
+//     }, 5000);
+// }
 
 document.addEventListener("DOMContentLoaded", () => {
     const toasts = document.querySelectorAll('.toast');
