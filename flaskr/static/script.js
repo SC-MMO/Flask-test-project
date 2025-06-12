e74e4479bd6f943c71c36dc48bcb327be34ba515
SSC-MMO <moritz.mummert@scaltel.de>
2025-06-12 16:07:32 +0200

    Removed old toast notification remover

--- flaskr/static/script.js
+++ flaskr/static/script.js
@@ -1,18 +1,5 @@
 const toastNotification = document.querySelector('#toastNotification');
 
-// const showToast = (message, isError = false) => {
-//     let toast = document.createElement('div');
-//     toast.classList.add('toast');
-//     if (isError) toast.classList.add('error');
-
-//     toast.innerHTML = message;
-//     toastNotification.appendChild(toast);
-
-//     setTimeout(() => {
-//         toast.remove();
-//     }, 5000);
-// }
-
 document.addEventListener("DOMContentLoaded", () => {
     const toasts = document.querySelectorAll('.toast');
     toasts.forEach(toast => {
