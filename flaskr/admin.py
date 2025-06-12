3ff7d26d0e83ca995d293c5b2b7aa3796ef42fce
SSC-MMO <moritz.mummert@scaltel.de>
2025-06-12 16:04:41 +0200

    Changed admin check to check if the admin permission of the current user is true

--- flaskr/admin.py
+++ flaskr/admin.py
@@ -1,19 +1,16 @@
 from flask import (
     Blueprint, redirect, render_template, session, url_for, abort
 )
-
+from flaskr.auth import login_required
+from .models import SiteUser
 
 admin_bp = Blueprint('admin', __name__)
 
 
 @admin_bp.route('/admin')
+@login_required
 def admin():
-    return f"WIP"
-    '''
-    username = session.get('username')
-    if str(username) == 'None':
-        return redirect(url_for('index'))
-    if username == 'admin':
+    User = SiteUser.objects(name=session.get('username')).first()
+    if User.permissions.get("admin"):
         return render_template('admin/admin.html')
     abort(401)
-    '''
