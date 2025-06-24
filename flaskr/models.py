c9b7bff9044611ad17fe15aaff429b909d49780d
SSC-MMO <moritz.mummert@scaltel.de>
2025-06-24 13:21:35 +0200

    made cascade better and moved some things around for the code logic to work

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,19 +1,23 @@
-from mongoengine import Document, StringField, DateTimeField, ReferenceField, EmailField, DictField, CASCADE
+from mongoengine import Document, StringField, DateTimeField, ReferenceField, EmailField, DictField, FileField, CASCADE
 from datetime import datetime
+
+class Image(Document):
+    name = StringField(required=True)
+    image_file = FileField(required=True)
+
+class Role(Document):
+    name = StringField(required=True)
+    permissions = DictField(required=True)
 
 class SiteUser(Document):
     name = StringField(required=True, max_length=100)
     address = EmailField()
     password = StringField(required=True)
-    role = ReferenceField(required=True, reverse_delete_rule=CASCADE)
-    permissions = DictField(required=True)
+    role = ReferenceField(Role, required=True, reverse_delete_rule=CASCADE)
+    profile_pic = ReferenceField(Image, required=True, delete_rule=CASCADE)
 
 class Post(Document):
     title = StringField(required=True, max_length=200)
     body = StringField(required=True)
-    created_at = DateTimeField(default=datetime.now())
+    created_at = DateTimeField(default=datetime.now)
     author = ReferenceField(SiteUser, required=True, reverse_delete_rule=CASCADE)
-
-class Role(Document):
-    name = StringField(required=True)
-    permissions = DictField(required=True)
